0caf6b7a6d86b92f33282d471ab4a5a89e620572
abhinavtyagi88 <132747922+abhinavtyagi88@users.noreply.github.com>
2024-10-07 00:14:34 +0530

    re-added index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  
+    <App />
+  
+  </BrowserRouter>
+);
+
+reportWebVitals();
