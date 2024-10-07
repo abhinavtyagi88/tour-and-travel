9dae5fcdf97a775731055f00bd7061081b4236c6
Abhinav Tyagi <Abhinavtyagiup@gmail.com>
2024-10-07 18:27:16 +0530

    bug fixed 2

--- Backend/Routers/visited.js
+++ Backend/Routers/visited.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const VisitedPlace = require('../models/VisitedModel');
 
+
+
 // Add a visited place
 router.post('/add', async (req, res) => {
   try {
